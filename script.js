ca7ee23436b1f01d2f844ee60f47c2d0e7ef6ed7
hanselkristanzen <hanselkristanzens@gmail.com>
2021-09-06 18:11:04 +0700

    Create window alert

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Version 1.2 Copyright: Hansel Kristanzen");
+document.getElementById("title").addEventListener("click", function(){
+    var newLine = "\n";
+    var msg = "LOCS";
+    msg += newLine;
+    msg += "Version 1.2";
+    msg += newLine;
+    msg += "Copyright: Hansel Kristanzen";
+    alert(msg);
+});
