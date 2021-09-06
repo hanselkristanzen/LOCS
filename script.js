f86dbb79136501ebff03ab5144f30e24293713d1
hanselkristanzen <hanselkristanzens@gmail.com>
2021-09-06 18:15:48 +0700

    bug fix
    Change on js window alert version

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("title").addEventListener("click", function(){
     var newLine = "\n";
     var msg = "LOCS";
     msg += newLine;
-    msg += "Version 1.2";
+    msg += "Version: 1.2";
     msg += newLine;
     msg += "Copyright: Hansel Kristanzen";
     alert(msg);
